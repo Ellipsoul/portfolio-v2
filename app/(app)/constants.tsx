3dc225f33f33d4a61c43d868c9842a35917888e5
Aron Teh <aronteh.chess@gmail.com>
2024-12-21 20:11:06 +0000

    Temporary Resume upload for historical records

--- app/(app)/constants.tsx
+++ app/(app)/constants.tsx
@@ -1,4 +1,7 @@
-import { ChatBubbleLeftEllipsisIcon, EnvelopeIcon } from "@heroicons/react/20/solid";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/20/solid";
 import { ForwardRefExoticComponent, SVGProps } from "react";
 
 export function classNames(...classes: string[]) {
@@ -85,12 +88,10 @@ export const social: NavigationSocialItem[] = [
     href: "https://twitter.com/aron_teh",
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path
-          d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646
+        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646
         4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27
         5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012
-        18.407a11.616 11.616 0 006.29 1.84"
-        />
+        18.407a11.616 11.616 0 006.29 1.84" />
       </svg>
     ),
     class: "w-16 h-16",
@@ -117,7 +118,7 @@ export const timeline: TimelineItem[] = [
     location: "Dublin, Ireland",
     time: "March 2024 - present",
     content: [
-      "In my current position, I build scalable API integrations for Stripe's Buy Now Pay Later partners. \
+      "In my current position, I build scalable API integrations for Stripe's Local Payment Methods (LPMs). \
       My work's tech stack consists primarily of Java, gRPC, MongoDB, and Ruby.",
     ],
   },
@@ -262,7 +263,8 @@ export const interests: Interest[] = [
       },
       {
         id: 2,
-        content: "Most recently, I have found great enjoyment in sharing my knowledge and experience with others.",
+        content:
+          "Most recently, I have found great enjoyment in sharing my knowledge and experience with others.",
       },
     ],
     logo: "/interest_logos/chesscom.png",
